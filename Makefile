c716d65a7a686c3aad49a0aac2ae45724094899a
Rami Husami <husamirami@gmail.com>
2023-08-07 16:31:58 +0300

    add metis transfer strategy deploymenty script

--- Makefile
+++ Makefile
@@ -8,10 +8,7 @@ update:; forge update
 # Build & test
 build  :; forge build --sizes --via-ir
 test   :; forge test -vvv
-test-sd-rewards :; forge test -vvv --match-contract EmissionTestSDPolygon
-test-stmatic-rewards :; forge test -vvv --match-contract EmissionTestSTMATICPolygon
-test-maticx-rewards :; forge test -vvv --match-contract EmissionTestMATICXPolygon
+test-sd-rewards :; forge test -vvv --match-contract EmissionTestMetisMetis
 
 # scripts
-deploy-sd-transfer-strategy :;  forge script scripts/RewardsConfigHelpers.s.sol:SDDeployTransferStrategy --rpc-url polygon --broadcast --legacy --ledger --mnemonic-indexes ${MNEMONIC_INDEX} --sender ${LEDGER_SENDER} --verify -vvvv
-deploy-stmatic-transfer-strategy :;  forge script scripts/RewardsConfigHelpers.s.sol:STMATICDeployTransferStrategy --rpc-url polygon --broadcast --legacy --ledger --mnemonic-indexes ${MNEMONIC_INDEX} --sender ${LEDGER_SENDER} --verify -vvvv
+deploy-metis-transfer-strategy :;  forge script scripts/RewardsConfigHelpers.s.sol:MetisDeployTransferStrategy --rpc-url metis --broadcast --legacy --private-key ${PRIVATE_KEY} --sender ${SENDER} --verify -vvvv
--- scripts/RewardsConfigHelpers.s.sol
+++ scripts/RewardsConfigHelpers.s.sol
@@ -3,32 +3,16 @@ pragma solidity ^0.8.0;
 
 import {Script} from 'forge-std/Script.sol';
 import {PullRewardsTransferStrategy} from 'aave-v3-periphery/contracts/rewards/transfer-strategies/PullRewardsTransferStrategy.sol';
-import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
+import {AaveV3Metis} from 'aave-address-book/AaveV3Metis.sol';
 
-contract SDDeployTransferStrategy is Script {
-  address internal constant EMISSION_ADMIN = 0x51358004cFe135E64453d7F6a0dC433CAba09A2a;
+contract MetisDeployTransferStrategy is Script {
+  address internal constant EMISSION_ADMIN = 0x97177cD80475f8b38945c1E77e12F0c9d50Ac84D;
   address internal constant REWARDS_VAULT = EMISSION_ADMIN;
 
   function run() external {
     vm.startBroadcast();
     new PullRewardsTransferStrategy(
-      AaveV3Polygon.DEFAULT_INCENTIVES_CONTROLLER,
-      EMISSION_ADMIN,
-      REWARDS_VAULT
-    );
-    vm.stopBroadcast();
-  }
-}
-
-/// @dev same to be used for MATICX, as they share rewards vault and emission admin
-contract STMATICDeployTransferStrategy is Script {
-  address internal constant REWARDS_VAULT = EMISSION_ADMIN;
-  address internal constant EMISSION_ADMIN = 0x0c54a0BCCF5079478a144dBae1AFcb4FEdf7b263;
-
-  function run() external {
-    vm.startBroadcast();
-    new PullRewardsTransferStrategy(
-      AaveV3Polygon.DEFAULT_INCENTIVES_CONTROLLER,
+      AaveV3Metis.DEFAULT_INCENTIVES_CONTROLLER,
       EMISSION_ADMIN,
       REWARDS_VAULT
     );
